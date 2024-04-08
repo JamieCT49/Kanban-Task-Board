4849be0e6a512c7341be2cb16f99798983ad447f
James <jamescamerontaylor20@gmail.com>
2024-04-08 23:51:47 +1000

    code so far

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,56 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    const newId = nextId;
+    nextId++;
+    localStorage.setItem('nextId', JSON.stringify(nextId));
+    return newId;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskCard = document.createElement("div");
+    taskCard.setAttribute("class", "taskCard");
+    let taskId = task.id;
+    taskCard.setAttribute('id', `taskId-${taskId}`);
+    const taskCardheader = document.createElement('div');
+    taskCardheader.setAttribute('class', 'taskCardheader');
+    const taskTitle = document.createElement('h4');
+    taskTitle.textContent = task.title;
+    taskTitle.setAttribute('id', 'taskTitle');
+    const taskDescription = document.createElement('p');
+    taskDescription.textContent = task.description;
+    taskDescription.setAttribute('id', 'taskDescription');
+    const taskDueDate = document.createElement('p');
+    taskDueDate.textContent = task.dueDate;
+    taskDueDate.setAttribute('id', 'taskDueDate');
+
+    taskCard.setAttribute('id', `taskId-${taskId}`)
+
+    taskCardheader.appendChild(taskTitle);
+    taskCard.appendChild(taskCardheader);
+    taskCard.appendChild(taskDescription);
+    taskCard.appendChild(taskDueDate);
+
+    const taskStatus = task.status
+    let taskColumn = document.getElementById(`${taskStatus}-cards`)
+    taskColumn.appendChild(taskCard);
+    renderTaskList();
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    $('.taskCard').draggable({
+        revert: 'invaild',
+        zIndex: 100,
+    });
 }
-
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    event.preventDefault();
+
+    const title = $('#taskTitle').val().trim();
 
 }
 
